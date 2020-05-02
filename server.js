209a7ab273dd307c84fd320c5ad1adf845a4d921
William Gartner <willgartner94@gmail.com>
2020-05-01 20:57:15 -0400

    Commit for project submission 1 Lesson 3

--- server.js
+++ server.js
@@ -2,18 +2,34 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express')
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors')
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+const server = app.listen(port, ()=>{console.log(`\x1b[36mServer running on localhost:\x1b[33m ${port}\x1b[0m`)});
+
+// POST to projectData
+app.post('/add', (req, res) => {
+    for (const property in req.body) {
+      projectData[property] = req.body[property];
+    }
+    res.send(projectData)
+});
+
+// GET projectData
+app.get('/data', (req, res) => {
+  res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,87 @@
 /* Global Variables */
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code(us)}&appid={your api key}&units=imperial
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const key = '&appid=7ef869df9318a59ad803c4e45df27c34&units=imperial';
+const gen = document.getElementById('generate');
+const zip = document.getElementById('zip');
+const feelings = document.getElementById('feelings');
+const zipError = document.getElementById('ziperror');
+let trueFeelings = '';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// GET Request for weather data
+const retrieveWeather = async (zip) => {
+    const request = await fetch(`${url}${zip}${key}`);
+    try {
+      const allData = await request.json();
+      // Temprature per API documentation
+      return parseInt(allData.main.temp);
+    }
+    catch(error) {
+      console.log("error: ", error);
+    }
+  };
+
+// POST data to projectData
+const postData = async ( url = '', data = {})=>{
+  const response = await fetch(url, {
+  method: 'POST', 
+  credentials: 'same-origin',
+  headers: {
+      'Content-Type': 'application/json',
+  },       
+  body: JSON.stringify(data), 
+});
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } 
+  catch(error) {
+    console.log("error: ", error);
+  }
+}
+  
+const addToUi = async () => {
+  const req = await fetch('/data');
+  try {
+    const projectData = await req.json();
+    document.getElementById('date').innerHTML=`Date: ${projectData.date}`;
+    document.getElementById('temp').innerHTML=`Temprature: ${projectData.temp}`;
+    document.getElementById('content').innerHTML=`Feelings: ${projectData.feelings}`;
+  } 
+  catch (error) {
+    console.log('error: ', error);    
+  }
+}
+  
+  // Run the main program loop
+  const executePageUpdate = () => {
+      const isValidZip = /(^\d{5}$)/.test(zip.value);
+      if(isValidZip){
+        if(feelings.value === '' || feelings.value === undefined || feelings.value === null){
+          trueFeelings = 'Must be feeling kind of empty!';
+        }
+        else {
+          trueFeelings = feelings.value;
+        }
+        retrieveWeather(zip.value)
+        .then(temp => {
+          postData('/add', { date: newDate, temp: temp, feelings: trueFeelings })
+        })
+        .then(() => {
+          addToUi();
+        });
+      }
+      else{        
+        zipError.innerHTML = 'Please enter a valid zip code format (5 digits)';
+      }
+  }
+
+
+// Add event listener for generate button
+gen.addEventListener('click', executePageUpdate);
+zip.addEventListener('change', () => {zipError.innerHTML = '';});
